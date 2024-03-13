590927dea61f2386824fab8d1f5ff14a2e5ca1b6
Qharny <cannycc1020@gmail.com>
2024-03-13 17:47:57 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   .vscode/settings.json
    	modified:   lib/main.dart
    	modified:   lib/screens/splash_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,7 @@ class MyApp extends StatelessWidget {
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // ignore: prefer_const_constructors
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
