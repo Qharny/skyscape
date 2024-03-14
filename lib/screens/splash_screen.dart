8f79a5d00db066febb80a5b554dee3c037d7bf7c
Qharny <cannycc1020@gmail.com>
2024-03-14 22:01:27 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 4 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   lib/main.dart
    	modified:   lib/screens/splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skyscape/constant/colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,7 +11,9 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: bgColor,
+    );
   }
 }
 
