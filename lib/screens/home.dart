68e413fa6356268aefc50d6e897bbd08208736c9
Qharny <cannycc1020@gmail.com>
2024-03-21 17:52:57 +0000

    modified

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:skyscape/constant/colors.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _isDarkMode = false;
+
+  void _toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // theme: ThemeData.light(),
+      // darkTheme: ThemeData.dark(),
       appBar: AppBar(
         // backgroundColor: bgColor,
         title: Row(
@@ -19,7 +34,15 @@ class Home extends StatelessWidget {
               },
             ),
             const Text('SKYSCAPE'),
-            IconButton(icon: const Icon(Icons.sunny), onPressed: () {})
+            IconButton(
+              icon: _isDarkMode
+                  ? const Icon(Icons.wb_sunny)
+                  : const Icon(
+                      Icons.nightlight_round,
+                      // color: white,
+                    ),
+              onPressed: _toggleTheme,
+            )
           ],
         ),
       ),
