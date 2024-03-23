69c697677bbaa279ee05e89ebbc9a1cc3aa5c73c
Qharny <cannycc1020@gmail.com>
2024-03-23 19:21:17 +0000

    modified

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,11 +11,12 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   bool switchThemes = false;
 
-  void _toggleTheme() {
-    setState(() {
-      switchThemes = !switchThemes;
-    });
-  }
+  // code to switch theme
+  // void _toggleTheme() {
+  //   setState(() {
+  //     switchThemes = !switchThemes;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -32,21 +33,30 @@ class _HomeState extends State<Home> {
                 print("The menu icon is pressed");
               },
             ),
-            const Text(
-              'SKYSCAPE',
-              style: TextStyle(
-                  // color: AppTheme.theme(
-                  //     context: context,
-                  //     darkTheme: Colors.black,
-                  //     lightTheme: Colors.black),
-                  ),
-            ),
-            IconButton(
-              icon: Icon(
-                switchThemes ? Icons.wb_sunny : Icons.nightlight_round,
-                color: switchThemes ? Colors.white : Colors.black,
-              ),
-              onPressed: _toggleTheme,
+            // const Text(
+            //   'SKYSCAPE',
+            //   style: TextStyle(
+            //       // color: AppTheme.theme(
+            //       //     context: context,
+            //       //     darkTheme: Colors.black,
+            //       //     lightTheme: Colors.black),
+            //       ),
+            // ),
+            Row(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      print("You pressed the search button");
+                    },
+                    icon: const Icon(Icons.search)),
+                // IconButton(
+                //   icon: const Icon(Icons.more_vert),
+                //   // onPressed: _toggleTheme,
+                //   onPressed: () {
+                //     print("More button is pressed");
+                //   },
+                // )
+              ],
             )
           ],
         ),
